37941ac6bd9ce1d97e25f6ea122f6a4a55d51ea8
FiiXxx <84270777+FiiXxx@users.noreply.github.com>
2021-08-07 13:46:59 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -14,37 +14,57 @@ module.exports = {
     .setColor("RANDOM")
    .setAuthor("Prefix [ $ ]","")
     .setDescription(` 
-\`Help Commands\`
+\ help
+List of all commands
+• ────── emoji_102 ────── •
+☢◢User Commands
++invite - +ping 
++stats - +support
++hosting - +docs
++howtohost - +djs
 
-**Music Command**
+• ────── RAES ────── •
+☢◢Music Commands
++addprevious - +addsimilar 
++autoplay - +join
++jump - +loob 
++loobqueue - +lyrics
++move - +moveme 
++nowplaying - +pause
++play(p) - +playlist
++playsc - +play skip 
++queue - +radio
++search - +shuffle 
++stop - +volume 
+────── Sixo ────── •
++8ball - +delete
++fact - +joke
++kiss - +meme
++rip - +shit
++say - +purge
+• ────── RAES ────── •
++addbotchat - +adddj
++prefix - +removebotchat 
++reset - settings - +setup 
++toggledjoinly - +toggleplaymssnge
 
-\` $paly , $stop , 
-$skip , $skipto , $volume \`
+• ────── emoji_97 ────── •
+☢◢Moderator Commands
++avatar - +botinfo - +help
++invite - +ping - +reactions
++serverinfo - +stats - +userinfo 
++uptime - +ban - +kick 
++embed - +slawmode
++unmute - +mute
++unwarn - +warn - +warnings 
 
-\` $queue , $loop , 
-$pause , $nowplaying \`
-
-\` $shuffle , $uptime ,
- $search , $remove \`
-
-\` $clip , $clips , 
-$lyrics , $move , $playlist \`
-
-
-
-
-**Info Command**
-
-
-\` $bot , $support ,$invite , $help, $ping \`
-
-
-
-     
-**[ invite ](https://discord.com/api/oauth2/authorize?client_id=866274289976213505&permissions=8&scope=bot)** , **[ support ](https://discord.gg/9qBvN4DhT5)**      
-  
-                                     
-
+• ────── FILTER────── •
+☢◢Owner Commands
++bassboost - +cleareq 
++clearfilter - +equalizer 
+• ────── emoji_107 ────── •
+☢◢Links
+Invite ("")    Support ("")
 `)
     
     helpEmbed.setTimestamp();
